604813cdb4257a69cde208bfbe6212bc7e152d76
MaheerJishan3 <mahir.mahibi@gmail.com>
2024-03-19 15:13:46 +0600

    file-changed

--- firPro/start.c
+++ firPro/start.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!");
-
-    printf("This is the second input");
-
-    printf("This is the third input");
+    printf("Changed content");
 
     return 0;
 }
